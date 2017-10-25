bfc5ebd61e35ab64eac734a8682b83347b856771
tony <382026180@qq.com>
2017-10-25 19:14:41 +0800

    采用es5编译

--- dist/vue-store.js
+++ dist/vue-store.js
@@ -2,10 +2,10 @@
  * Created by dcpai on 2017/7/4.
  */
 (function () {
-  let global = window || this;
-  let ENGIE_LOCAL = 'localStorage';
-  let ENGIE_SESSION = 'sessionStorage';
-  const Store = {
+  var global = window || this;
+  var ENGIE_LOCAL = 'localStorage';
+  var ENGIE_SESSION = 'sessionStorage';
+  var Store = {
     engine: ENGIE_LOCAL,
     set local(str) {
       this.engine = ENGIE_LOCAL;
@@ -27,26 +27,26 @@
       global[Store.engine].setItem(inKey, JSON.stringify(inValue));
     },
     get: function (inKey) {
-      let value = global[Store.engine].getItem(inKey);
+      var value = global[Store.engine].getItem(inKey);
       return value;
     },
     sets: function (inObject) {
-      for (let inKey in inObject) {
+      for (var inKey in inObject) {
         Store.set(inKey, inObject[inKey]);
       }
     },
     gets: function (inKeys) {
-      let result = {};
-      let keys = inKeys || [];
-      let i = 0, key;
-      let storeEngine = global[Store.engine];
+      var result = {};
+      var keys = inKeys || [];
+      var i = 0, key;
+      var storeEngine = global[Store.engine];
       if (keys.length === 0) {
         for (i = 0; i < storeEngine.length; i++) {
           key = storeEngine.key(i);
           keys.push(key);
         }
       }
-      for (let k in keys) {
+      for (var k in keys) {
         result[keys[k]] = Store.get(keys[k]);
       }
       return result;
@@ -64,5 +64,7 @@
       }
     }
   };
-  module.exports = Store;
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Store;
+  }
 }());
